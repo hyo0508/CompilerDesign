b7a04901978906f5ccb22103b49e11932ccd5806
hyo0508 <2018008168@hanyang.ac.kr>
2022-12-20 23:51:50 +0900

    Semantic Init

--- 4_/analyze.c
+++ 4_/analyze.c
@@ -59,7 +59,6 @@ static void insertNode(TreeNode *t)
     switch (t->kind.stmt)
     {
     case AssignK:
-    case ReadK:
       if (st_lookup(t->attr.name) == -1)
         /* not yet in table, so treat as new definition */
         st_insert(t->attr.name, t->lineno, location++);
@@ -127,7 +126,7 @@ static void checkNode(TreeNode *t)
           (t->child[1]->type != Integer))
         typeError(t, "Op applied to non-integer");
       if ((t->attr.op == EQ) || (t->attr.op == LT))
-        t->type = Boolean;
+        t->type = Integer;
       else
         t->type = Integer;
       break;
@@ -149,14 +148,6 @@ static void checkNode(TreeNode *t)
     case AssignK:
       if (t->child[0]->type != Integer)
         typeError(t->child[0], "assignment of non-integer value");
-      break;
-    case WriteK:
-      if (t->child[0]->type != Integer)
-        typeError(t->child[0], "write of non-integer value");
-      break;
-    case RepeatK:
-      if (t->child[1]->type == Integer)
-        typeError(t->child[1], "repeat test is not Boolean");
       break;
     default:
       break;
